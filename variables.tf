fa34d7d7ea4ce0ab43b06a8922f1172a9b0b6687
Prav-Akula <pravs.akula@outlook.com>
2026-04-22 16:27:33 -0400

    runner

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z0258322TB5OSJHYCLEQ"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "prav4cloud.online"
+  description = "description"
+}
